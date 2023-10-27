85a8ac023543d036c18e35fcbea54b57d3a3a99f
anish-karnik <114944809+anish-karnik@users.noreply.github.com>
2023-10-27 11:27:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from './pages/Home/Navbar';
+import Home from './pages/Home/Homescreen';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+<Navbar/>
+<Routes>
+  <Route path="/" element={ <Home/>} ></Route> 
+</Routes>
+
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe(){
+    return (
+        <section id="AboutMe" className="about--section">
+          <div className="about--section--img">
+            <img src="./img/about-me.png" alt="About Me" />
+          </div>
+          <div className="hero--section--content--box about--section--box">
+            <div className="hero--section--content">
+              <p className="section--title">About</p>
+              <h1 className="skills-section--heading">About Me</h1>
+              <p className="hero--section-description">
+              I am a third-year undergraduate student at the Indian Institute of Technology pursuing Computer Science and Engineering. I am eager to learn new things and motivated for applying my knowledge to relevant solutions.
+              </p>
+              <p className="hero--section-description">
+              I want to explore this diverse field, where there is always something new to uncover and evolving technology to explore.
+              </p>
+            </div>
+          </div>
+        </section>
+      );    
+}
